dd28407cacc4969271b4df9622ab536241680554
PrasantaDebnath25 <prasantadebnath.2013@gmail.com>
2024-02-17 13:56:59 +0530

    Test page added

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+export default function Test() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
